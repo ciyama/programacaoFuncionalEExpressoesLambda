197e784ebaf52342fb973375dd72edee03c22acb
Celio Iyama <celio.iyama@gmail.com>
2021-07-18 17:30:47 -0300

    Function parte 5 - Java

--- comparator/src/application/Program.java
+++ comparator/src/application/Program.java
@@ -3,11 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.UperCaseName;
 
 public class Program {
 	
@@ -24,7 +22,6 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-	
 	
 		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
 	
