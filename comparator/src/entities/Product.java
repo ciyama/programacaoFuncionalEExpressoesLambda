89b7db611704054330689d72f546fd9350cfb352
Celio Iyama <celio.iyama@gmail.com>
2021-07-18 16:10:00 -0300

    Consumer parte 2 - Java

--- comparator/src/entities/Product.java
+++ comparator/src/entities/Product.java
@@ -25,8 +25,8 @@ public class Product {
 		this.price = price;
 	}
 	
-	public boolean nonStaticProductPredicate() {
-		return price >= 100.0;
+	public static void StaticPriceUpdate(Product p) {
+		p.setPrice(p.getPrice() * 1.1);
 	}
 
 	@Override
