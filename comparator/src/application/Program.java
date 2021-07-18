c233485d35c85f007a6b77c8e40c56c780a45cf4
Celio Iyama <celio.iyama@gmail.com>
2021-07-18 16:34:00 -0300

    Function parte 1 - Java

--- comparator/src/application/Program.java
+++ comparator/src/application/Program.java
@@ -3,9 +3,10 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UperCaseName;
 
 public class Program {
 	
@@ -22,16 +23,10 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		
-		double factor = 1.1;
-		
-		//Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
 	
-		
-		//list.forEach(cons);
-		list.forEach(p -> p.setPrice(p.getPrice() * factor));
-		
-		list.forEach(System.out::println);
+		List<String> names = list.stream().map(new UperCaseName()).collect(Collectors.toList());
+	
+		names.forEach(System.out::println);
 		
 		}
 		
