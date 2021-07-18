d108fe0e81febcb82dd58ab5e372f53341f26b2c
Celio Iyama <celio.iyama@gmail.com>
2021-07-18 18:12:11 -0300

    Function parte 8 Predicate - Java

--- function2/src/application/Program.java
+++ function2/src/application/Program.java
@@ -17,11 +17,11 @@ public class Program {
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
-		list.add(new Product("HD Case", 80.90));
+		list.add(new Product("M2 HD Case", 80.90));
 		
 		ProductService ps = new ProductService();
 		
-		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
+		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'M');
 		
 		System.out.println("Sum = " + String.format("%.2f", sum));
 	}
